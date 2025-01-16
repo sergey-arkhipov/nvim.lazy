de8cacc53f5fccab8992d23b1179f8d18e2796d6
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2025-01-16 09:32:28 +0300

    Add popup and windows setup. Moice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,50 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		cmdline = {
+			enabled = true, -- enables the Noice cmdline UI
+			view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+			opts = {}, -- global options for the cmdline. See section on views
+		},
+
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 5,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = "auto",
+				},
+			},
+			popupmenu = {
+				relative = "editor",
+				position = {
+					row = 8,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = 10,
+				},
+				border = {
+					style = "rounded",
+					padding = { 0, 1 },
+				},
+				win_options = {
+					winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+				},
+			},
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+}
