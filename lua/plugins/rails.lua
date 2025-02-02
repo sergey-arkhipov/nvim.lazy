5e2b176b93cf1554a034e5e271370628e9776e59
Sergey Arkhipov <sergey-arkhipov@yandex.ru>
2025-02-02 22:18:36 +0300

    Add rails-nvim

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -1,3 +1,21 @@
 return {
-  { "tpope/vim-rails" },
+  "sergey-arkhipov/rails-nvim",
+  name = "rails-nvim",
+  ft = "ruby",
+  config = function()
+    require("rails-nvim").setup({
+      spec_dir = "rspec",
+      service_dir = "services",
+    })
+  end,
+  keys = {
+
+    { "gr", "", desc = "Rails fast move" },
+    { "grc", "<cmd>GC<cr>", desc = "Controller" },
+    { "grh", "<cmd>GH<cr>", desc = "Helper" },
+    { "grm", "<cmd>GM<cr>", desc = "Model" },
+    { "grr", "<cmd>GR<cr>", desc = "Route" },
+    { "grt", "<cmd>GS<cr>", desc = "Spec" },
+    { "grv", "<cmd>GV<cr>", desc = "View" },
+  },
 }
